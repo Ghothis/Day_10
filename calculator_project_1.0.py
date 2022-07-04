9675233095c3baf0dd73829374fa759c1c4cdf20
Diony J. Adorno <96499721+Ghothis@users.noreply.github.com>
2022-07-03 22:04:50 -0400

    Create calculator_project_1.0.py
    Original copy of initial calculator project.

---
+++ calculator_project_1.0.py
@@ -0,0 +1,10 @@
+# TODO #1: Create various functions with outputs that contains the basic aritmetic operations in math
+
+
+
+
+# TODO #2: Creates a Dictionary that contains the aritmectic symbols as key and function name as pair values
+
+
+
+# TODO #3: Creates a function that holds the entire calculator program
